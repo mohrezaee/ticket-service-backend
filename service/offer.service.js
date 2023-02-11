c9b978959704729051fd90359d0e9ac8bfc1ee81
s.ahmadzadeh <s.ahmadzadeh@emofid.com>
2023-02-11 19:05:26 +0330

    refactor: add date checking in getOffer

--- service/offer.service.js
+++ service/offer.service.js
@@ -1,3 +1,4 @@
+import { Sequelize } from "sequelize";
 import { Offer } from "../model/offer.model.js";
 
 
@@ -7,22 +8,20 @@ export async function createOffer(data) {
 }
 
 export async function getOffer(filter) {
-    console.log(filter)
     let origin = filter.origin
     let destination = filter.destination
-    let departure_local_time = filter.departure_local_time
+    let departure = new Date(filter.departure_local_time)
+    let departure_tomarrow = new Date(filter.departure_local_time)
+    departure_tomarrow.setDate(departure_tomarrow.getDate() + 1)
 
     const response = await Offer.findAll({
         where: {
             origin,
-            destination
+            destination,
+            departure_local_time: {
+                [Sequelize.Op.between]: [departure, departure_tomarrow]
+            }
         }
     });
     return response
-}
-
-function sameDay(d1, d2) {
-    return d1.getFullYear() === d2.getFullYear() &&
-      d1.getMonth() === d2.getMonth() &&
-      d1.getDate() === d2.getDate();
 }
